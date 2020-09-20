f026e3417ae9a0cc4fcd5ac800721805438de2a7
davidlei <davidleitw@gmail.com>
2020-09-20 13:51:49 +0800

    新增上傳單張影像輸出yolo辨識結果api, 還沒做回傳影像動作

--- backend/detect.py
+++ backend/detect.py
@@ -1,11 +1,20 @@
-from fastapi import  WebSocket, APIRouter
+from fastapi import  WebSocket, APIRouter, UploadFile, File
 from fastapi.responses import HTMLResponse
 
+import time
 import numpy as np 
 import cv2
 from yolov4.tf import YOLOv4
 
+class yolo(object):
+    def __init__(self):
+        self.yolo = YOLOv4()
+        self.yolo.classes = 'backend/coco.names'
+        self.yolo.make_model()
+        self.yolo.load_weights("backend/yolov4.weights", weights_type="yolo")
+
 router = APIRouter()
+Yolo = yolo()
 
 @router.get('/')
 async def hello():
@@ -18,14 +27,14 @@ async def websocket_connection(websocket: WebSocket):
     yolo.classes = 'coco.names'
     yolo.make_model()
     yolo.load_weights("yolov4.weights", weights_type="yolo")
-    
+
     while True:
         _frame = await websocket.receive_bytes()
         nparray = np.frombuffer(_frame, np.uint8)
         image = cv2.imdecode(nparray, cv2.IMREAD_UNCHANGED)
           
-        data = yolo.predict(frame=image)
-        result = yolo.draw_bboxes(image, data)
+        boxes = yolo.predict(frame=image)
+        result = yolo.draw_bboxes(image, boxes)
         cv2.imshow('frame', result)
         await websocket.send_text("keep connection")
 
@@ -34,3 +43,21 @@ async def websocket_connection(websocket: WebSocket):
 
     cv2.destroyAllWindows()
 
+@router.post('/yolov4')
+async def yolov4_from_singleImage(files: UploadFile = File(...)):
+    try: 
+        file = await files.read()
+        image = np.fromstring(file, np.uint8)
+        image = cv2.imdecode(image, cv2.IMREAD_UNCHANGED)
+
+        boxes = Yolo.yolo.predict(frame=image)
+        result = Yolo.yolo.draw_bboxes(image, boxes)    
+        cv2.imshow('image', result)
+        cv2.waitKey(0)
+        time.sleep(10)
+        cv2.destroyAllWindows()   
+        return {"message": "got your image, and show the image on the server"}
+    except Exception as e:
+        print("error = ", e)
+        return {"message": str(e)}
+    
