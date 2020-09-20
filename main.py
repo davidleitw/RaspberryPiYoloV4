fec4344089f64baf85e9b1ae1cee77a7012e7300
davidlei <davidleitw@gmail.com>
2020-09-20 13:52:33 +0800

    main

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 from fastapi import FastAPI
 from backend.detect import router
 from yolov4.tf import YOLOv4
+import uvicorn
+import cv2
 
 app = FastAPI()
 app.include_router(router)
 
-# yolo = YOLOv4()
-
-# yolo.classes = "backend/coco.names"
-
-# yolo.make_model()
-# yolo.load_weights("backend/yolov4.weights", weights_type="yolo")
-
-# yolo.inference(media_path="dog.jpg")
+# if __name__ == '__main__':
+#     uvicorn.run(app="main:app", host="127.0.0.1", port=8080, reload=True, debug=True)
