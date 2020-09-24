47bec18d824febdbbf5792f522a7a08f7ee4b543
davidlei <davidleitw@gmail.com>
2020-09-24 11:46:27 +0800

    連續傳送測試yolo速度

--- raspberryPI/upload_file.py
+++ raspberryPI/upload_file.py
@@ -2,10 +2,11 @@ import requests
 
 url = "http://127.0.0.1:8000/yolov4"
 
-file = open('kite.jpg', 'rb')
+for i in range(20):
+    file = open('kite.jpg', 'rb')
 
 # image = cv2.imread('dog.jpg')
-files = {'files': file}
-req = requests.post(url, files=files)
-json = req.json()
-print(json)
+    files = {'files': file}
+    req = requests.post(url, files=files)
+    json = req.json()
+    print(json)
